24b8d72e772dc27bf62c6a22053835ff7635ab3d
ikede-divine-keno <ikededivine@gmail.com>
2023-07-25 07:31:18 -0700

    0-bubble_sort.c, 0-main.c and bubble added

---
+++ 0-bubble_sort.c
@@ -0,0 +1,26 @@
+#include "sort.h"
+/**
+ * bubble_sort - sort array elements from min to max value
+ * @array: array
+ * 
+ */
+void bubble_sort(int *array, size_t size)
+{
+
+	size_t a, idx, ptr = 0;
+
+	if (size < 2)
+		return;
+
+	for (a = 0; a < size; a++)
+		for (idx = 0; idx < size; idx++)
+		{
+			if (array[idx] > array[idx + 1])
+			{
+			ptr = array[idx];
+			array[idx] = array[idx + 1];
+			array[idx + 1] = ptr;
+			print_array(array, size);
+			}
+		}
+}
