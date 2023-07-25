e8f1d75d67f360ef32b2033f58c476b215b9cf96
ikede-divine-keno <ikededivine@gmail.com>
2023-07-25 09:31:49 -0700

    1-insertion_sort_list.c, 1-main.c and insertion files added

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sorts a linked list using insertion sort
+ *
+ * 
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *ptr;
+
+	if (list == NULL || *list == NULL)
+		return;
+
+	for (ptr = (*list)->next; ptr != NULL; ptr = ptr->next)
+	{
+		while (ptr->prev != NULL && ptr->prev->n > ptr->n)
+		{
+			ptr->prev->next = ptr->next;
+
+			if (ptr->next)
+				ptr->next->prev = ptr->prev;
+
+			ptr->next = ptr->prev;
+			ptr->prev = ptr->next->prev;
+			ptr->next->prev = ptr;
+
+			if (!ptr->prev)
+				*list = ptr;
+			else
+				ptr->prev->next = ptr;
+			print_list(*list);
+		}
+	}
+}
